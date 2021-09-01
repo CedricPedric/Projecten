2518deb4327e868ca142417c5ef6cce568825f3f
Cedric Francis <89778393+CedricPedric@users.noreply.github.com>
2021-09-01 13:47:15 +0200

    Add files via upload

---
+++ Demo1.py
@@ -0,0 +1,5 @@
+print('Hello World') #output to terminal
+name = input("What's your name:") #input from terminal
+print('Hello,' + name)
+age = input('How old are you:')
+print(age +'! ' + 'I am only 1 second old!')
